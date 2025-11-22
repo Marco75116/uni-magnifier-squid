818dd1dbf07c031665be3f4aa035e46f6f911f11
Marco75116 <ourradourm@gmail.com>
2025-11-22 22:30:53 +0100

    refactor: add normalizeAddress

--- src/utils/constants/networkconfigs/eth.networkconfig.ts
+++ src/utils/constants/networkconfigs/eth.networkconfig.ts
@@ -1,4 +1,5 @@
 import type { NetworkConfig } from "../network.constant";
+import { normalizeAddress } from "../../helpers/global.helper";
 
 export const ethNetworkConfig: NetworkConfig = {
   chainId: 1,
@@ -7,19 +8,19 @@ export const ethNetworkConfig: NetworkConfig = {
   finalityConfirmation: 12,
   contracts: {
     PoolManager: {
-      address: "0x000000000004444c5dc75cB358380D2e3dE08A90",
+      address: normalizeAddress("0x000000000004444c5dc75cB358380D2e3dE08A90"),
       range: { from: 21688329 },
     },
     PositionDiscriptor: {
-      address: "0xd1428Ba554F4C8450b763a0B2040A4935c63f06C",
+      address: normalizeAddress("0xd1428Ba554F4C8450b763a0B2040A4935c63f06C"),
       range: { from: 21689088 },
     },
     PositionManager: {
-      address: "0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e",
+      address: normalizeAddress("0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e"),
       range: { from: 21689089 },
     },
     Permit2: {
-      address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      address: normalizeAddress("0x000000000022D473030F116dDEE9F6B43aC78BA3"),
       range: { from: 15986406 },
     },
   },
